0d50aa838a569a5f1cee5f4633a3ce9870e58830
Chris Eun <eunchristopher@gmail.com>
2023-01-21 14:16:45 -0800

    sending index and show data

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+console.log('__dirname', __dirname + '/views')
+
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
 
 // ROUTES
 app.get('/', (req, res) => {
